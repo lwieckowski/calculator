ecfcdfc1e624fe4fe29e065a4203c865b6d4104a
lwieckowski <lwieckowski@outlook.com>
2023-02-14 21:07:51 +0100

    Rename button to key

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ function App() {
   return (
     <div>
       <div id="display">00000000</div>
-      <div id="button-container">
-        <div className="button dark-grey">C</div>
-        <div className="button dark-grey">&plusmn;</div>
-        <div className="button dark-grey">%</div>
-        <div className="button orange">&divide;</div>
-        <div className="button light-grey">7</div>
-        <div className="button light-grey">8</div>
-        <div className="button light-grey">9</div>
-        <div className="button orange">&times;</div>
-        <div className="button light-grey">4</div>
-        <div className="button light-grey">5</div>
-        <div className="button light-grey">6</div>
-        <div className="button orange">-</div>
-        <div className="button light-grey">1</div>
-        <div className="button light-grey">2</div>
-        <div className="button light-grey">3</div>
-        <div className="button orange">+</div>
-        <div className="button light-grey" id="button-0">0</div>
-        <div className="button light-grey">.</div>
-        <div className="button orange">=</div>
+      <div id="key-container">
+        <div className="key dark-grey">C</div>
+        <div className="key dark-grey">&plusmn;</div>
+        <div className="key dark-grey">%</div>
+        <div className="key orange">&divide;</div>
+        <div className="key light-grey">7</div>
+        <div className="key light-grey">8</div>
+        <div className="key light-grey">9</div>
+        <div className="key orange">&times;</div>
+        <div className="key light-grey">4</div>
+        <div className="key light-grey">5</div>
+        <div className="key light-grey">6</div>
+        <div className="key orange">-</div>
+        <div className="key light-grey">1</div>
+        <div className="key light-grey">2</div>
+        <div className="key light-grey">3</div>
+        <div className="key orange">+</div>
+        <div className="key light-grey" id="key-0">0</div>
+        <div className="key light-grey">.</div>
+        <div className="key orange">=</div>
       </div>
     </div>
   );
