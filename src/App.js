3149b7f24212f7c5bf8cd6ee6a8d030e84b91d7f
lwieckowski <lwieckowski@outlook.com>
2023-02-14 20:57:25 +0100

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="display">00000000</div>
+      <div id="button-container">
+        <div className="button dark-grey">C</div>
+        <div className="button dark-grey">&plusmn;</div>
+        <div className="button dark-grey">%</div>
+        <div className="button orange">&divide;</div>
+        <div className="button light-grey">7</div>
+        <div className="button light-grey">8</div>
+        <div className="button light-grey">9</div>
+        <div className="button orange">&times;</div>
+        <div className="button light-grey">4</div>
+        <div className="button light-grey">5</div>
+        <div className="button light-grey">6</div>
+        <div className="button orange">-</div>
+        <div className="button light-grey">1</div>
+        <div className="button light-grey">2</div>
+        <div className="button light-grey">3</div>
+        <div className="button orange">+</div>
+        <div className="button light-grey" id="button-0">0</div>
+        <div className="button light-grey">.</div>
+        <div className="button orange">=</div>
+      </div>
     </div>
   );
 }
