2e1d4503347ceeb0a28eab8f989be3ddeff97cd0
lwieckowski <lwieckowski@outlook.com>
2023-03-09 11:03:42 +0100

    Add test for clicking on numeric key 0

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,9 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import App from './App';
 
-describe('user can see a display', () => {
+describe('Calculator display', () => {
   
-  test('showing the current number entered or the last result', () => {
+  test('shows the current number entered or the last result', () => {
     render(<App />);
     const currentValue = '00000001';
     const display = screen.getByRole('textbox', { id: "display"});
@@ -13,7 +13,7 @@ describe('user can see a display', () => {
     expect(display).toHaveValue(currentValue);
   });
 
-  test('that is read only', () => {
+  test('is read only', () => {
     render(<App />);
     const displayValue = '12345678';
     const userInput = '5';
@@ -25,9 +25,9 @@ describe('user can see a display', () => {
 
 });
 
-describe('user can see a keypad', () => {
+describe('Calculator keypad', () => {
   
-  test('with all calculator keys', () => {
+  test('has all required keys', () => {
     render(<App />);
     const keyNames = [
       '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -37,6 +37,18 @@ describe('user can see a keypad', () => {
       const key = screen.getByRole('button', {name: keyName});
       expect(key).toBeInTheDocument();
     }
+  });
+
+});
+
+  describe('Calculator usage', () => {
+
+  test('user can click on key 0 and it will show up on display', () => {
+    render(<App />);
+    const display = screen.getByRole('textbox', { id: "display"});
+    const key = screen.getByRole('button', {name: "0"});
+    userEvent.click(key);
+    expect(display).toHaveValue("0");
   });
 
 });
