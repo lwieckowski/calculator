8b3c4794b2cb5a9fc227128824207ef9147fa556
lwieckowski <lwieckowski@outlook.com>
2023-03-09 12:58:25 +0100

    Implement numeric key sequences and clear key

--- src/App.js
+++ src/App.js
@@ -4,34 +4,45 @@ import './App.css';
 function App() {
   const [display, setDisplay] = useState("");
 
-  const handleClick = (e) => {
-    const digit = e.target.name;
-    setDisplay(digit);
-  }
+  const handleNumericKey = (e) => {
+    const key = e.target.name;
+    if (display.length < 8) {
+      setDisplay(display => {
+        if (key === '.' && display.includes(key)) {
+          return display;
+        }
+        return display + key;
+      });
+    };
+  };
+
+  const handleClearKey = (e) => {
+    setDisplay("");
+  };
 
   return (
     <div id="app-container">
       <div>
         <input id="display" disabled value={display}></input>
         <div id="keypad">
-          <FunctionKey name="C">C</FunctionKey>
+          <FunctionKey name="C" handleClick={handleClearKey}>C</FunctionKey>
           <FunctionKey name="±">&plusmn;</FunctionKey>
           <FunctionKey name="%">%</FunctionKey>
           <OperatorKey name="/">&divide;</OperatorKey>
-          <NumericKey name="7" handleClick={handleClick}>7</NumericKey>
-          <NumericKey name="8" handleClick={handleClick}>8</NumericKey>
-          <NumericKey name="9" handleClick={handleClick}>9</NumericKey>
+          <NumericKey name="7" handleClick={handleNumericKey}>7</NumericKey>
+          <NumericKey name="8" handleClick={handleNumericKey}>8</NumericKey>
+          <NumericKey name="9" handleClick={handleNumericKey}>9</NumericKey>
           <OperatorKey name="×">&times;</OperatorKey>
-          <NumericKey name="4" handleClick={handleClick}>4</NumericKey>
-          <NumericKey name="5" handleClick={handleClick}>5</NumericKey>
-          <NumericKey name="6" handleClick={handleClick}>6</NumericKey>
+          <NumericKey name="4" handleClick={handleNumericKey}>4</NumericKey>
+          <NumericKey name="5" handleClick={handleNumericKey}>5</NumericKey>
+          <NumericKey name="6" handleClick={handleNumericKey}>6</NumericKey>
           <OperatorKey name="-">-</OperatorKey>
-          <NumericKey name="1" handleClick={handleClick}>1</NumericKey>
-          <NumericKey name="2" handleClick={handleClick}>2</NumericKey>
-          <NumericKey name="3" handleClick={handleClick}>3</NumericKey>
+          <NumericKey name="1" handleClick={handleNumericKey}>1</NumericKey>
+          <NumericKey name="2" handleClick={handleNumericKey}>2</NumericKey>
+          <NumericKey name="3" handleClick={handleNumericKey}>3</NumericKey>
           <OperatorKey name="+">+</OperatorKey>
-          <NumericKey name="0" handleClick={handleClick}>0</NumericKey>
-          <NumericKey name="." handleClick={handleClick}>.</NumericKey>
+          <NumericKey name="0" handleClick={handleNumericKey}>0</NumericKey>
+          <NumericKey name="." handleClick={handleNumericKey}>.</NumericKey>
           <OperatorKey name="=">=</OperatorKey>
         </div>
       </div>
@@ -39,15 +50,23 @@ function App() {
   );
 }
 
-function OperatorKey({ name, children }) {
+function OperatorKey({ name, children, handleClick }) {
   return (
-    <button className="key orange" name={name} >{children}</button>
+    <button
+      className="key orange"
+      name={name}
+      onClick={handleClick}
+    >{children}</button>
   );
 }
 
-function FunctionKey({ name, children }) {
+function FunctionKey({ name, children, handleClick }) {
   return (
-    <button className="key dark-grey" name={name}>{children}</button>
+    <button
+      className="key dark-grey"
+      name={name}
+      onClick={handleClick}
+    >{children}</button>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -32,15 +32,34 @@ describe('Calculator keypad', () => {
 });
 
 describe('Calculator usage', () => {
-
-  const numericKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-
+  
   describe('when display is empty', () => {
-    for (const key of numericKeys) {
-      test(`user can click on key ${key} and it will show up on display`, () => {
+    const keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    for (const key of keys) {
+      test(`user can click on key ${key} and it will show on display`, () => {
         testNumericKeyClick(key);
       });
     }
+  });
+
+  test('user can enter numbers as sequences up to 8 digits long', () => {
+    render(<App />);
+    const keys = ['0', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    for (const keyName of keys) {
+      const key = screen.getByRole('button', {name: keyName});
+      userEvent.click(key);
+    }
+    const display = screen.getByRole('textbox', { id: "display"});
+    expect(display).toHaveValue('0.123456');
+  });
+
+  test('user can enter only one decimal separator', () => {
+    render(<App />);
+    const key = screen.getByRole('button', {name: '.'});
+    userEvent.click(key);
+    userEvent.click(key);
+    const display = screen.getByRole('textbox', { id: "display"});
+    expect(display).toHaveValue('.');
   });
 
   const testNumericKeyClick = (keyName) => {
