a7c0b8b76a99f14dd7abf5f407b2fafd6542136b
lwieckowski <lwieckowski@outlook.com>
2023-03-12 13:05:16 +0100

    Refactor to use React context

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, createContext } from "react";
 import { Keypad } from "./Keypad";
 import { Display } from "./Display";
 
@@ -13,84 +13,129 @@ const OPERATORS = {
   "÷": (a, b) => a / b,
 };
 
+const initialState = {
+  currentInput: "0",
+  operator: null,
+  operand1: 0,
+  operand2: 0,
+  isNewEntry: true,
+};
+
+const CalculatorContext = createContext(initialState);
+
+function calculatorReducer(state, action) {
+  switch (action.type) {
+    case "INPUT":
+      if (state.isNewEntry || state.currentInput.length < DEFAULT_DIGITS) {
+        const updatedInput = processInput(
+          action.payload,
+          state.currentInput,
+          state.isNewEntry
+        );
+        return { ...state, currentInput: updatedInput, operand2: Number(updatedInput), isNewEntry: false };
+      } else {
+        return state;
+      }
+    case "OPERATOR":
+      if (state.operator) {
+        const result = OPERATORS[state.operator](
+          state.operand1,
+          state.operand2
+        );
+        return {
+          ...state,
+          currentInput: String(result),
+          operator: action.payload,
+          operand1: result,
+          operand2: 0,
+          isNewEntry: true,
+        };
+      } else {
+        return {
+          ...state,
+          operator: action.payload,
+          operand1: state.operand2,
+          operand2: 0,
+          isNewEntry: true,
+        };
+      }
+    case "EQUALS":
+      if (state.operator) {
+        const result = OPERATORS[state.operator](
+          state.operand1,
+          state.operand2
+        );
+        return {
+          ...state,
+          currentInput: String(result),
+          operator: null,
+          operand1: result,
+          operand2: 0,
+          isNewEntry: true,
+        };
+      } else {
+        return { ...state, isNewEntry: true };
+      }
+    case "FUNCTION":
+      const FUNCTIONS = {
+        "%": () =>
+          state.operator
+            ? state.operand1 * state.operand2 * 0.01
+            : state.operand2 * 0.01,
+        "±": () => -Number(state.currentInput),
+      };
+      const updatedInput = FUNCTIONS[action.payload]();
+      return { ...state, currentInput: String(updatedInput), operand2: updatedInput };
+    case "CLEAR":
+      return { ...initialState };
+    default:
+      return state;
+  }
+}
+
+const processInput = (key, prevInput, isNewEntry) => {
+  if (isNewEntry) {
+    return key === "." ? "0." : key;
+  } else {
+    return key === "." && prevInput.includes(key) ? prevInput : prevInput + key;
+  }
+};
+
 export function Calculator() {
-  const [operand1, setOperand1] = useState(0);
-  const [operand2, setOperand2] = useState(0);
-  const [operator, setOperator] = useState(null);
-  const [display, setDisplay] = useState("0");
-  const [isNewEntry, setIsNewEntry] = useState(true);
+  const [state, dispatch] = useReducer(calculatorReducer, initialState);
 
   const handleInputKey = (e) => {
-    const key = e.target.value;
-    const newValue = getNewDisplayValue(key, isNewEntry);
-    if (isNewEntry || display.length < DEFAULT_DIGITS) {
-      setDisplay(newValue);
-      setOperand2(Number(newValue));
-      setIsNewEntry(false);
-    }
-  };
-
-  const getNewDisplayValue = (key, isNewEntry) => {
-    if (isNewEntry) {
-      return key === "." ? "0." : key;
-    } else {
-      return key === "." && display.includes(key) ? display : display + key;
-    }
+    dispatch({ type: "INPUT", payload: e.target.value });
   };
 
   const handleClearKey = () => {
-    setOperand1(0);
-    setOperand2(0);
-    setOperator(null);
-    setDisplay("0");
-    setIsNewEntry(true);
+    dispatch({ type: "CLEAR" });
   };
 
   const handleOperatorKey = (e) => {
-    if (operator) {
-      const result = OPERATORS[operator](operand1, operand2);
-      setOperand1(result);
-      setDisplay(result);
-    } else {
-      setOperand1(operand2);
-    }
-    setOperator(e.target.value);
-    setOperand2(0);
-    setIsNewEntry(true);
+    dispatch({ type: "OPERATOR", payload: e.target.value });
   };
 
   const handleEqualsKey = () => {
-    if (operator) {
-      const result = OPERATORS[operator](operand1, operand2);
-      setOperand1(result);
-      setOperand2(0);
-      setDisplay(String(result));
-      setOperator(null);
-    }
-    setIsNewEntry(true);
+    dispatch({ type: "EQUALS" });
   };
 
   const handleFunctionKey = (e) => {
-    const functionKey = e.target.value;
-    const FUNCTIONS = {
-      "%": () => (operator ? operand1 * operand2 * 0.01 : operand2 * 0.01),
-      "±": () => -Number(display),
-    };
-    const result = FUNCTIONS[functionKey]();
-    setDisplay(String(result));
-    setOperand2(result);
+    dispatch({ type: "FUNCTION", payload: e.target.value });
   };
 
   return (
-    <div>
-      <Display value={display} />
-      <Keypad
-        handleClearKey={handleClearKey}
-        handleFunctionKey={handleFunctionKey}
-        handleInputKey={handleInputKey}
-        handleOperatorKey={handleOperatorKey}
-        handleEqualsKey={handleEqualsKey}
-      />
-    </div>
+    <CalculatorContext.Provider value={state}>
+      <div>
+        <Display value={state.currentInput} />
+        <Keypad
+          handleClearKey={handleClearKey}
+          handleFunctionKey={handleFunctionKey}
+          handleInputKey={handleInputKey}
+          handleOperatorKey={handleOperatorKey}
+          handleEqualsKey={handleEqualsKey}
+        />
+      </div>
+    </CalculatorContext.Provider>
   );
 }
